345d9fac8bc40d6b33a88e23fe36e9f0cc085b8f
joshiCoding <joshi.mohit1221@gmail.com>
2020-07-25 23:31:11 +0530

    menu functionality added

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+
+//----------------for hamburger menu
+const menu = document.querySelector('.hamburger-menu-options');
+const menuOpenBtn = document.querySelector('.hamburger-open-btn');
+const menuCloseBtn = document.querySelector('.hamburger-close-btn');
+
+menuOpenBtn.addEventListener('click', e=>{
+    menu.classList.add('hamburger-menu-options-open');
+
+    document.querySelector('body').addEventListener('click' , e=>{
+        if(!e.target.classList.contains('hamburger-menu-options') && menu.clientWidth !== 0){
+            menu.classList.remove('hamburger-menu-options-open');
+        }
+    })
+})
+
+menuCloseBtn.addEventListener('click', e=>{
+    menu.classList.remove('hamburger-menu-options-open');
+    
+})
